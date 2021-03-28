41c8b7d265be0a8df39e823522d356f0bc83d62f
Guillaume B <bellanger.guillaume77@gmail.com>
2021-03-28 16:59:43 +0200

    routes terminées

---
+++ backend/routes/comment.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+
+const commentCtrl = require("../controllers/comment");
+
+router.get("/message/:msgId/comment", commentCtrl.allComments);
+router.post("/message/:msgId/comment", commentCtrl.postComment);
+router.put(
+  "/message/:msgId/comment/:commentId",
+  auth,
+  commentCtrl.updateComment
+);
+router.delete(
+  "/message/:msgId/comment/:commentId",
+  auth,
+  commentCtrl.deleteComment
+);
+
+module.exports = router;
